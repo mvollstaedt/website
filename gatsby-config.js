e5eaa1eff01e8835b24c9b557491114f4f04740a
mvollstaedt <max.vollstaedt@gmx.de>
2020-09-25 18:45:30 +0200

    Set up gatsby blog project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Maximilian Vollstädt - A Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@mvollstaedt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: 'pages',
+      },
+    },
+    'gatsby-plugin-catch-links',
   ],
 }
